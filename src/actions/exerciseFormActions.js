16f7bd07f4ff5b9184003b7c8ad26a2f7a9ab663
Greg <Ghughes610@gmail.com>
2021-06-06 21:03:45 -0600

    Form Update

---
+++ src/actions/exerciseFormActions.js
@@ -0,0 +1,16 @@
+const BASE_URL = ""
+
+export const save = (exercise) => {
+    // const options = {
+    //     method: "POST",
+    //     Headers:
+    //     {
+    //         'content-type': 'application/json',
+    //         'accept': 'application/json'
+    //     },
+    //     body: JSON.stringify(exercise)
+    // }
+    // fetch(`${BASE_URL}/v1/save`, options)
+    console.log(exercise)
+}
+
